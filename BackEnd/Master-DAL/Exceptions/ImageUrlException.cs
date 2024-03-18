6cb727aa822aa6d322f3b06ed3f8885ea195355b
Suman Rai <officeproj111@gmail.com>
2024-03-18 23:52:11 +0545

    Custom Exception on ArticlesRepo

---
+++ BackEnd/Master-DAL/Exceptions/ImageUrlException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master_DAL.Exceptions
+{
+    public class ImageUrlException : Exception
+    {
+        public ImageUrlException()
+        {
+            
+        }
+
+        public ImageUrlException(string message) : base(message)
+        {
+            
+        }
+
+        public ImageUrlException(string message, Exception inner): base(message, inner)
+        {
+            
+        }
+
+        public ImageUrlException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context): base(info, context)
+        {
+            
+        }
+    }
+}
