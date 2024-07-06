a7ec115bd944b571133bdafb3125a39816dcaa7a
Suman Rai <officeproj111@gmail.com>
2024-07-06 07:56:25 +0545

    DASDSA

--- BackEnd/Master-DAL/DbContext/ApplicationDbContext.cs
+++ BackEnd/Master-DAL/DbContext/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Master_DAL.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,16 @@ namespace Master_DAL.DbContext
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+            if(databaseCreator != null)
+            {
+                if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                if(!databaseCreator.HasTables()) databaseCreator.CreateTables();
+
+            }
+        }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
