8f5e1703a19c9e0bd4f7d6eaca1709a93ef7f393
Suman Rai <officeproj111@gmail.com>
2024-09-06 11:51:07 +0545

    ASDASDSA

--- FrontEndArticles/src/types/IPaginationResponse.tsx
+++
@@ -1,7 +0,0 @@
-export interface IPaginationResponse<T> {
-    Items: T[];
-    PageIndex: number;
-    PageSize: number;
-    TotalItems: number;
-    TotalPages: number;
-}
--- FrontEndArticles/src/types/IUser.tsx
+++
@@ -1,5 +0,0 @@
-export interface IUser {
-    Id: string;
-    UserName: string;
-    Email: string;
-}
